3d151628be96718f1e0bf4d877b6242de2ffc505
gsdkira <larreakelly@gmail.com>
2022-04-26 11:56:56 -0400

    added PostWidget PostCard and Categories to Homepage as components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,30 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { PostCard, Categories, PostWidget } from '../components'
+import {PostCard, Categories, PostWidget} from '../components'
 
 const posts = [
-  {title: 'Next JS Testing', excerpt: 'Learn NextJS'},
-  {title: 'Next JS React with Tailwind', excerpt: 'Learn React NextJs w/TailWind CSS'},
+  { title: 'Next JS Testing', excerpt: 'Learn NextJS' },
+  { title: 'Next JS React with Tailwind', excerpt: 'Learn React NextJs w/TailWind CSS' },
 ]
 
 const Home: NextPage = () => {
   return (
-    <div className="container mx-auto px-10 mb-8 ">
+    <div className="container mx-auto px-10 mb-8">
       <Head>
         <title>Next.JS Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-      <div className="lg:col-span-8 col-span-1">
-      {posts.map((post) => <PostCard post={post} key={post.title}/>
-      )}
-      </div>
-       <div className="lg:col=span-4 col-span-1">
-        <div className="lg:sticky relative top-8">
-
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post) => <PostCard post={post} key={post.title} />)}
         </div>
+        <div className="lg:col=span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget /> 
+            <Categories />
+          </div>
         </div>
       </div>
-     
     </div>
   )
 }
