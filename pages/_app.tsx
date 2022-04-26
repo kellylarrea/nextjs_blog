2e2bcd89ad4e68d44e214bf7fa50640ba32eabe3
gsdkira <larreakelly@gmail.com>
2022-04-26 15:39:29 -0400

    added header component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
+import React, { useEffect, useState } from 'react'
+import { Layout } from '../components'
 import 'tailwindcss/tailwind.css'
 import '../styles/global.scss'
+
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <Layout>
+  <Component {...pageProps} />
+  
+  </Layout>
+  )
 }
 
 export default MyApp
